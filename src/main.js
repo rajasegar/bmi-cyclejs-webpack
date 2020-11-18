0751c30ba7d7813d1d87d22e39ce4c32988ed49f
Rajasegar Chandran <rajasegar.c@gmail.com>
2020-11-18 12:37:54 +0530

    chore: refactor to MVI Pattern

--- src/main.js
+++ src/main.js
@@ -3,45 +3,70 @@ import { makeDOMDriver } from '@cycle/dom';
 import { run } from '@cycle/run';
 import Snabbdom from 'snabbdom-pragma';
 
-function main(sources) {
-  const changeWeight$ = sources.DOM.select('.weight')
-    .events('input')
-    .map(ev => ev.target.value);
-
-  const changeHeight$ = sources.DOM.select('.height')
-    .events('input')
-    .map(ev => ev.target.value);
-
-  const weight$ = changeWeight$.startWith(70);
-  const height$ = changeHeight$.startWith(170);
-
-  const state$ = xs.combine(weight$, height$)
-    .map(([weight, height]) => {
-      const heightMeters = height * 0.01;
-      const bmi =  Math.round(weight / (heightMeters * heightMeters));
-      return { weight, height, bmi };
-    });
-
-  const vdom$ = state$.map(({weight, height, bmi})  => {
-    return (
+function renderWeightSlider(weight) {
+  return (
     <div>
+      Weight {weight}kg
+      <input className="weight" type="range" min="40" max="140" value={weight}/>
+        </div>
+  );
+}
+
+function renderHeightSlider(height) {
+  return (
+    <div>
+      Height {height}cm
+      <input className="height" type="range" min="140" max="210" value={height}/>
+        </div>
+  );
+}
+
+function bmi(weight, height) {
+  const heightMeters = height * 0.01;
+  const bmi =  Math.round(weight / (heightMeters * heightMeters));
+}
+
+function view(state$) {
+  return state$.map(({weight, height, bmi})  => {
+    return (
       <div>
-        Weight {weight}kg
-        <input className="weight" type="range" min="40" max="140"/>
-      </div>
-      <div>
-        Height {height}cm
-        <input className="height" type="range" min="140" max="210"/>
-      </div>
-      <h2>BMI is {bmi}</h2>
-    </div>
+        {renderWeightSlider(weight)}
+        {renderHeightSlider(height)}
+        <h2>BMI is {bmi}</h2>
+          </div>
     );
   });
 
-  const sinks = {
-    DOM: vdom$
+}
+
+function model(actions) {
+
+  const weight$ = actions.changeWeight$.startWith(70);
+  const height$ = actions.changeHeight$.startWith(170);
+
+  return xs.combine(weight$, height$)
+    .map(([weight, height]) => {
+      return { weight, height, bmi: bmi(weight, height) };
+    });
+}
+
+function intent(domSource) {
+
+  const changeWeight$ = domSource.select('.weight')
+    .events('input')
+    .map(ev => ev.target.value);
+
+  const changeHeight$ = domSource.select('.height')
+    .events('input')
+    .map(ev => ev.target.value);
+
+  return { changeWeight$, changeHeight$ }
+}
+
+function main(sources) {
+  return {
+    DOM: view(model(intent(sources.DOM)))
   };
-  return sinks;
 }
 
 run(main, {
